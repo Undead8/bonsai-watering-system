754b4af023097e267d8eb819a204422b7679f2eb
Youri <cousineau915@hotmail.com>
2019-08-20 20:46:34 -0400

    Add winter mode
    Add Winter mode and its PID controlled heating system.

--- bonsai.ino
+++ bonsai.ino
@@ -1,7 +1,8 @@
 #include <Wire.h>
 #include <SerLCD.h> // http://librarymanager/All#SparkFun_SerLCD
-#include <DFRobot_SHT20.h> // https://github.com/DFRobot/DFRobot_SHT20 Default I2C address is 0x40
+#include <DFRobot_SHT20.h> // https://github.com/DFRobot/DFRobot_SHT20
 #include <NewPing.h> // https://bitbucket.org/teckel12/arduino-new-ping/wiki/Home
+#include <PID_v1.h> // https://github.com/br3ttb/Arduino-PID-Library
 
 // Pin constants
 const unsigned int PUMP_HEATER_PIN = 1;
@@ -15,18 +16,27 @@ const unsigned int NO_WATER_LVL = 6;
 const unsigned int WATERING_TIME = 5000;
 const unsigned int WATERING_PAUSE = 120000;
 const unsigned int MEASUREMENT_DELAY = 600000; // Delay between temp/humidity measures
-const float MIN_HUMIDITY = 50.0;
+const double MIN_HUMIDITY = 50.0;
 
-// Global variables
+// PID variables and constants
+const double KP = 1.0;
+const double KI = 1.0;
+const double KD = 1.0;
+const double TEMP_TARGET = 0;
+double input, output, setpoint;
+
+// Other global variables
 bool no_water = false;
-unsigned long last_measurement;
-int humidity;
-int temperature;
+unsigned long last_measurement = 0;
+double humidity;
+double temperature;
 bool winter_mode;
 
-SerLCD lcd; // Initialize the library with default I2C address 0x72
-DFRobot_SHT20 sht20;
+
+SerLCD lcd; // Default I2C address 0x72
+DFRobot_SHT20 sht20; // Default I2C address is 0x40
 NewPing sonar(SONAR_TRIGGER_PIN, SONAR_ECHO_PIN, 100);
+PID myPID(&input, &output, &setpoint, KP, KI, KD, DIRECT);
 
 
 void setup() {
@@ -46,19 +56,26 @@ void setup() {
   pinMode(WINTER_MODE_BTN, INPUT_PULLUP);
   winter_mode = digitalRead(WINTER_MODE_BTN) == LOW;
 
-  // Delay to make sure everything is init, then check temp and humidity.
-  delay(100);
+  // Check and display temperature and humidity
   checkTempHum();
+
+  // Activate PID if in winter mode
+  if (winter_mode){
+    input = temperature;
+    setpoint = TEMP_TARGET;
+    myPID.SetMode(AUTOMATIC);
+  }
 }
 
 
 void loop() {
 
-  unsigned long time_since = millis() - last_measurement;
+  // Do nothing until enough time has passed since the last measurement
+  while ((millis() - last_measurement) < MEASUREMENT_DELAY) { delay(1); }
 
-  if (winter_mode) {
-    // WINTER MODE HERE
-  } else if (time_since > MEASUREMENT_DELAY) {
+  if (winter_mode) { 
+    heatSoil();
+  } else {
     waterPlants();
   }
 }
@@ -66,16 +83,16 @@ void loop() {
 
 // Check temperature and humidity, then display on screen
 void checkTempHum() {
-  temperature = static_cast<int>(sht20.readTemperature());
-  humidity = static_cast<int>(sht20.readHumidity());
+  temperature = sht20.readTemperature();
+  humidity = sht20.readHumidity();
   last_measurement = millis();
   lcd.setCursor(0, 0);
   lcd.print("Temp: ");
-  lcd.print(temperature);
+  lcd.print(static_cast<int>(temperature));
   lcd.print("C");
   lcd.setCursor(0, 1);
   lcd.print("Hum: ");
-  lcd.print(humidity);
+  lcd.print(static_cast<int>(humidity));
   lcd.print("%");
 }
 
@@ -87,6 +104,7 @@ bool enoughWater() {
   int sonar_ping = sonar.ping_cm();
   
   if (sonar_ping >= NO_WATER_LVL) {
+    
     // Print on screen no water and change backlight to red
     lcd.setBacklight(255, 51, 0); //Set backlight to red
     lcd.setCursor(11, 0);
@@ -95,7 +113,9 @@ bool enoughWater() {
     lcd.print("WATER");
     no_water = true;
     return false;
+    
   } else if (sonar_ping >= LOW_WATER_LVL) {
+    
     // Print on screen low water and change backlight to yellow
     lcd.setBacklight(255, 255, 0); //Set backlight to yellow
     lcd.setCursor(11, 0);
@@ -103,13 +123,14 @@ bool enoughWater() {
     lcd.setCursor(10, 1);
     lcd.print("WATER");
     return true;
+    
   } else {
     return true;
   }
 }
 
 
-// Start the pump for ms
+// Start the pump for WATERING_TIME duration
 void pumpWater() {
   digitalWrite(PUMP_HEATER_PIN, HIGH);
   delay(WATERING_TIME);
@@ -117,7 +138,7 @@ void pumpWater() {
 }
 
 
-// Check humidity and water plants, unless there is not enough water
+// Check humidity and water plants if needed and if there is enough water
 void waterPlants() {
   checkTempHum();
   
@@ -126,4 +147,13 @@ void waterPlants() {
     delay(WATERING_PAUSE);
     checkTempHum();
   }
+}
+
+
+// Check the temperature, compute PID output and PWM the PID output
+void heatSoil() {
+  checkTempHum();
+  input = temperature;
+  myPID.Compute();
+  analogWrite(PUMP_HEATER_PIN, output);
 }
