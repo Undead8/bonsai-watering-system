1feef9d309dad90376140d7d5d31047322263e13
Youri <cousineau915@hotmail.com>
2020-08-04 09:05:22 -0400

    Replace winter mode by manual mode

--- bonsai.ino
+++ bonsai.ino
@@ -1,14 +1,12 @@
 #include <Wire.h>
 #include <SerLCD.h> // http://librarymanager/All#SparkFun_SerLCD
-#include <DFRobot_SHT20.h> // https://github.com/DFRobot/DFRobot_SHT20
 #include <I2CSoilMoistureSensor.h> // https://github.com/Apollon77/I2CSoilMoistureSensor
 #include <NewPing.h> // https://bitbucket.org/teckel12/arduino-new-ping/wiki/Home
-#include <PID_v1.h> // https://github.com/br3ttb/Arduino-PID-Library
 
 // Pin constants
 const unsigned int SONAR_TRIGGER_PIN = 6;
 const unsigned int SONAR_ECHO_PIN = 7;
-const unsigned int WINTER_MODE_BTN = 4;
+const unsigned int MANUAL_MODE_BTN = 4;
 const unsigned int PUMP_HEATER_PIN = 5;
 
 // Other customizable constants
@@ -16,137 +14,32 @@ const unsigned int FULL_WATER_LVL = 285; // Sonar ping in uS (57 uS = 1 cm) for 
 const unsigned int LOW_WATER_LVL = 627; // Sonar ping in uS (57 uS = 1 cm) for low water warning
 const unsigned int NO_WATER_LVL = 741; // Sonar ping in uS (57 uS = 1 cm) for no water
 const unsigned int WATERING_TIME = 5000; // Duration in ms that the pump will be active when watering
-const unsigned int WATERING_PAUSE = 120000; // Minimum wait time between watering in ms during watering cycle
 const unsigned long MEASUREMENT_DELAY = 300000; // Delay in ms between temp/humidity measures
 const double MIN_HUMIDITY = 213.0; // Minimum humidity in Capacitance that can be reached before watering
-
-// PID variables and constants
-const double KP = 1.0;
-const double KI = 1.0;
-const double KD = 1.0;
-const double TEMP_TARGET = 0;
-double input, output, setpoint;
 
 // Other global variables
 unsigned long last_measurement;
 double humidity, temperature;
-bool winter_mode;
+bool manual_mode;
 String status_message;
 unsigned long rgb_backlight = 0xffffff; // White
 
 // Library objects
 SerLCD lcd; // Default I2C address 0x72
-DFRobot_SHT20 sht20; // Default I2C address is 0x40
 I2CSoilMoistureSensor sensor; // Default I2C address is 0x20
 NewPing sonar(SONAR_TRIGGER_PIN, SONAR_ECHO_PIN, 100);
-PID myPID(&input, &output, &setpoint, KP, KI, KD, DIRECT);
-
-
-void setup() {
-  
-  Serial.begin(9600);
-  Wire.begin();
-
-  // Set pin modes
-  pinMode(WINTER_MODE_BTN, INPUT_PULLUP);
-  pinMode(PUMP_HEATER_PIN, OUTPUT);
-  digitalWrite(PUMP_HEATER_PIN, LOW);
-
-  // Set summer or winter mode
-  winter_mode = digitalRead(WINTER_MODE_BTN) == LOW;
-  Serial.print("Winter mode: ");
-  Serial.println(winter_mode);
-
-  // LCD - Init
-  Serial.println("Initializing LCD");
-  lcd.begin(Wire); //Set up the LCD for I2C communication
-  delay(1000);
-  lcd.setFastBacklight(rgb_backlight);
-  lcd.clear(); //Clear the display - this moves the cursor to home position as well
-  lcd.print("  ROBO-BONSAI");
-  lcd.setCursor(0, 1);
-  lcd.print("   MODE ");
-  if (winter_mode) {
-    lcd.print("HIVER");
-    rgb_backlight = 0x99ccff; // Blue
-    lcd.setFastBacklight(rgb_backlight);
-  } else {
-    lcd.print("ETE");
-    rgb_backlight = 0xccffcc; // Green
-    lcd.setFastBacklight(rgb_backlight);
-  }
-  Serial.println("LCD initialized");
-
-  // Sensors/PID - Init for winter and summer mode
-  if (winter_mode) {
-
-    // Activate PID
-    input = temperature;
-    setpoint = TEMP_TARGET;
-    myPID.SetMode(AUTOMATIC);
-
-    // SHT20 - Init
-    Serial.println("Initializing SHT20");
-    sht20.initSHT20();
-    delay(1000);
-    Serial.println("SHT20 initialized");
-    
-  } else {
-    
-    // I2CSoilMoisture - Init
-    Serial.println("Initializing I2CSoilMoisture");
-    sensor.begin();
-    delay(1000);
-    Serial.println("I2CSoilMoisture initialized");
-  }
-
-  // Perform first cycle
-  delay(3000);
-  if (winter_mode) {
-    heatSoil();
-  } else {
-    waterPlants();
-  }
-  displayLCD();
-
-  Serial.println("Setup completed");
-}
-
-
-void loop() {
-
-  // Do nothing until enough time has passed since the last measurement
-  while ((millis() - last_measurement) < MEASUREMENT_DELAY) { delay(1); }
-
-  if (winter_mode) {
-    heatSoil();
-  } else {
-    waterPlants();
-  }
-  displayLCD();
-}
 
 
 // Check temperature and humidity
 void checkTempHum() {
-
-  if (winter_mode) {
     
-    // Use sht20 to measure temperature and humidity
-    temperature = sht20.readTemperature(); // In Celsius
-    humidity = sht20.readHumidity(); // In percent
-    
-  } else {
-    
-    // Use I2CSoilMoisture to measure temperature and humidity
-    sensor.getCapacitance();
-    while (sensor.isBusy()) delay(50);
-    sensor.getTemperature();
-    while (sensor.isBusy()) delay(50);
-    humidity = sensor.getCapacitance(); // In Capacitance
-    temperature = sensor.getTemperature() / 10.0; // In Celsius 
-  }
-  
+  // Use I2CSoilMoisture to measure temperature and humidity
+  sensor.getCapacitance();
+  while (sensor.isBusy()) delay(50);
+  sensor.getTemperature();
+  while (sensor.isBusy()) delay(50);
+  humidity = sensor.getCapacitance(); // In Capacitance
+  temperature = sensor.getTemperature() / 10.0; // In Celsius   
   last_measurement = millis();
 
   // Serial debug
@@ -212,9 +105,6 @@ bool enoughWater() {
     // Serial debug
     Serial.println("Enough water");
 
-    // LCD variables
-    rgb_backlight = 0xffffff; // White
-    
     return true;
   }
 }
@@ -222,13 +112,20 @@ bool enoughWater() {
 
 // Start the pump for WATERING_TIME duration
 void pumpWater() {
-  
+
+  // Pump water for WATERING_TIME ms
   digitalWrite(PUMP_HEATER_PIN, HIGH);
   Serial.println("Pump on");
   delay(WATERING_TIME);
   digitalWrite(PUMP_HEATER_PIN, LOW);
   Serial.println("Pump off");
+
+  // Update temp, humidity and water lvl.
+  checkTempHum();
+  enoughWater();
+  displayLCD();
 }
+
 
 // Display information on LCD
 void displayLCD() {
@@ -249,45 +146,79 @@ void displayLCD() {
   lcd.print(status_message);
 }
 
-// RGB to hex converter
-unsigned long createRGB(int r, int g, int b)
-{   
-    return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
-}
 
-
-// Check humidity and water plants if needed and if there is enough water
-void waterPlants() {
-  checkTempHum();
-
-  // The pump will be activated many times until MIN_HUMIDITY is reached
-  while (enoughWater() && humidity < MIN_HUMIDITY) {
-    Serial.println("Watering loop started");
-    pumpWater();
-    delay(WATERING_PAUSE);
-    checkTempHum();
-  }
-}
-
-
-// Check the temperature, compute PID output and PWM the PID output to the heating pad
-void heatSoil() {
-
-  // The temperature is the PID input, the PID output is the PWM value (0-255)
-  checkTempHum();
-  input = temperature;
-  myPID.Compute();
-  analogWrite(PUMP_HEATER_PIN, output);
+void setup() {
   
-  // Variables for LCD
-  unsigned long rgb_output = createRGB(255, 255 - output, 255 - output);
-  rgb_backlight = (rgb_output); // From white to red depending of output
-  unsigned int heat_percent = map(output, 0, 255, 0, 100);
-  status_message = String(" CHAUFFAGE " + String(heat_percent) + "%");
+  Serial.begin(9600);
+  Wire.begin();
 
-  // Serial debug
-  Serial.print("Heat PID output: ");
-  Serial.println(output);
-  Serial.print("Heat %: ");
-  Serial.println(heat_percent);
+  // Set pin modes
+  pinMode(MANUAL_MODE_BTN, INPUT_PULLUP);
+  pinMode(PUMP_HEATER_PIN, OUTPUT);
+  digitalWrite(PUMP_HEATER_PIN, LOW);
+
+  // Set auto or manual mode
+  manual_mode = digitalRead(MANUAL_MODE_BTN) == LOW;
+  Serial.print("Manual mode: ");
+  Serial.println(manual_mode);
+
+  // LCD - Init
+  Serial.println("Initializing LCD");
+  lcd.begin(Wire);
+  delay(1000);
+  lcd.clear();
+  lcd.print("  ROBO-BONSAI");
+  lcd.setCursor(0, 1);
+  lcd.print("   MODE ");
+  if (manual_mode) {
+    lcd.print("MANUEL");
+    rgb_backlight = 0x99ccff; // Blue
+    lcd.setFastBacklight(rgb_backlight);
+  } else {
+    lcd.print("AUTO");
+    rgb_backlight = 0xccffcc; // Green
+    lcd.setFastBacklight(rgb_backlight);
+  }
+  Serial.println("LCD initialized");
+  
+  // I2CSoilMoisture - Init
+  Serial.println("Initializing I2CSoilMoisture");
+  sensor.begin();
+  delay(1000);
+  Serial.println("I2CSoilMoisture initialized");  
+
+  // Delay for splash screen
+  delay(2000);
+
+  // Check temp, humidity and water and display on LCD
+  checkTempHum();
+  enoughWater();
+  displayLCD();
+
+  Serial.println("Setup completed");
+}
+
+
+void loop() {
+
+  if (manual_mode) {
+
+    // Pump water non-stop until there is no more water
+    while (enoughWater()) { pumpWater(); }
+
+    // When there is no water, update temp and humidity every MEASUREMENT_DELAY
+    while ((millis() - last_measurement) < MEASUREMENT_DELAY) { delay(1); }
+    checkTempHum();
+    displayLCD();
+       
+  } else {
+    
+    // Update temp and humidity every MEASUREMENT_DELAY
+    while ((millis() - last_measurement) < MEASUREMENT_DELAY) { delay(1); }
+    checkTempHum();
+    displayLCD();
+
+    // Pump water once if there is water and humidity is low
+    if (enoughWater() && humidity < MIN_HUMIDITY) { pumpWater(); } 
+  }
 }
